20b7fb5324cc32af62f9d451860c2148ab459636
Thaina2405 <thaina.silva199757@gmail.com>
2020-11-05 20:50:50 -0300

    segurançaBlog

--- blogPessoalnew/src/app/cadastro/cadastro.component.ts
+++ blogPessoalnew/src/app/cadastro/cadastro.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { User } from '../model/User';
+import { AuthService } from '../service/auth.service';
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  user: User = new User();
+  senha: string;
+  constructor(private authService: AuthService, private router: Router) {}
+  ngOnInit() {}
+  conferirSenha(event:any){
+    this.senha=event.target.value
   }
-
+  cadastrar() {
+    if(this.senha === this.user.senha){
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp;
+      });
+      this.router.navigate(['/login']);
+      alert ('Cadastro efetuado com sucesso!')
+    }
+    else{
+      alert ('Suas senhas não conferem')
+    }
+  }
 }
